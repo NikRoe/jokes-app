5d7eb6c5cfec376fa4e6fbdcaf462bbd01b1587b
Niklas Röttger <niklas.roettger@outlook.de>
2022-03-17 18:16:15 +0100

    add delete and edit buttons

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import EditButton from "../components/EditButton/EditButton";
 
 export default function Home() {
   const [jokes, setJokes] = useState();
+  //
 
   useEffect(() => {
     async function fetchJokes() {
@@ -16,14 +18,36 @@ export default function Home() {
       }
     }
     fetchJokes();
-  }, []);
+  }, [jokes]);
+
+  async function handleDelete(id) {
+    console.log(id);
+    const response = await fetch(`/api/jokes/${id}`, { method: "DELETE" });
+    const deletedJoke = await response.json();
+    if (response.ok) {
+      alert(`Data with ID ${deletedJoke.data._id} has been deleted`);
+    } else {
+      alert(`oops - ${deletedJoke.error}`);
+    }
+  }
 
   return (
     <StyledDiv>
       <h1>Jokes-App</h1>
       <ul>
         {jokes ? (
-          jokes.map((joke) => <li key={joke._id}>{joke.text}</li>)
+          jokes.map((joke) => (
+            <StyledLi key={joke._id}>
+              <span>{joke.text}</span>
+              <div>
+                <EditButton joke={joke}></EditButton>
+
+                <button onClick={(item) => handleDelete(joke._id)}>
+                  Delete
+                </button>
+              </div>
+            </StyledLi>
+          ))
         ) : (
           <p>Loading...</p>
         )}
@@ -41,4 +65,11 @@ const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
+`;
+
+const StyledLi = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 0.5rem;
 `;
